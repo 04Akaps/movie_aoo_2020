c385a66546dfe39229fe797619333ab1131d1cda
유호진 <sdl155@jbnu.ac.kr>
2021-09-25 14:20:34 +0900

    수정 완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Detail from "./components/Detail";
-import Navigation from "./components/Navigation";
 import Home from "./routers/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
-      {/* <Navigation /> */}
-
       <Switch>
         <Route exact path="/">
           <Home />
